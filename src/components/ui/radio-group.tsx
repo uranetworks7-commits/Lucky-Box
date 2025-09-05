56cf224fcd9b4ad3d480132146c18c72b86391ec
Utkarsh Singh <utkarshsingh2008smp@gmail.com>
2025-09-05 17:23:10 +0000

    flushSync was called from inside a lifecycle method. React cannot flush

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -34,9 +34,7 @@ const RadioGroupItem = React.forwardRef<
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <div className="flex items-center justify-center">
-         <Circle className="h-2.5 w-2.5 fill-current text-current" />
-        </div>
+        <Circle className="h-2.5 w-2.5 fill-current text-current" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
