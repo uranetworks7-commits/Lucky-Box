a85c21e20b48a4af1ec5624abc358d0c082f6dec
Utkarsh Singh <utkarshsingh2008smp@gmail.com>
2025-09-05 17:37:51 +0000

    Try fixing this error: `Console Error: flushSync was called from inside

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -33,7 +33,7 @@ const RadioGroupItem = React.forwardRef<
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+      <RadioGroupPrimitive.Indicator>
         <Circle className="h-2.5 w-2.5 fill-current text-current" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
